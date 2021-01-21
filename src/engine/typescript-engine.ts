a31c2bda509806b2139c7b80004edceb66e908e0
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-01-21 00:32:23 -0500

    add comments support

--- src/engine/typescript-engine.ts
+++ src/engine/typescript-engine.ts
@@ -2,57 +2,73 @@ import { pascalCase } from 'change-case';
 import { OpenAPIV3Schema } from '../types';
 import { OpenAPIV3 } from 'openapi-types';
 
-function fromSchemaObjectToTypeScripType(data: OpenAPIV3.BaseSchemaObject) {
+function fromSchemaObjectToTypeScripType(data: OpenAPIV3.BaseSchemaObject): { output: string; comment?: string } {
   const typeOutput: string[] = [];
 
   if ('properties' in data) {
     const propertiesOutput = Object.entries(data.properties ?? {}).map((entry) => {
-      return `${entry[0]}: ${toTypeScripType(entry[1])}`;
+      const typeOutput = toTypeScripType(entry[1]);
+      return `${typeOutput.comment ?? '\n'}${entry[0]}: ${typeOutput.output}`;
     });
 
     typeOutput.push(`{ ${propertiesOutput.join(';\n')} }`);
   }
 
-  return typeOutput.join('');
+  return { output: typeOutput.join('') };
 }
 
-function toTypeScripType(data: OpenAPIV3Schema) {
+function toTypeScripType(data: OpenAPIV3Schema): { output: string; comment?: string } {
   if ('$ref' in data) {
     // TODO: Fix $ref object type
-    return `any /* is a Ref:${data.$ref} */`;
+    return {
+      comment: `
+      /**
+        * Is a Ref of ${data.$ref}
+        */\n`,
+      output: 'any',
+    };
   }
 
   if (data.type === 'boolean') {
-    return 'boolean';
+    return { output: 'boolean' };
   }
 
   if (data.type === 'number') {
-    return 'number';
+    return { output: 'number' };
   }
 
   if (data.type === 'string') {
-    return 'string';
+    return { output: 'string' };
   }
 
   if (data.type === 'integer') {
-    return 'number';
+    return { output: 'number' };
   }
 
   if (data.type === 'array') {
     // TODO: Fix array type
-    return 'Array<any>';
+    return { output: 'Array<any>' };
   }
 
   if (data.type === 'object') {
     return fromSchemaObjectToTypeScripType(data);
   }
 
-  return `any /* ${data.type} is unknown */`;
+  return {
+    comment: `
+    /**
+      * ${data.type} is unknown
+      */\n`,
+    output: 'any',
+  };
 }
 
 export function generateTypes(args: { schemaName: string; schemaObject: OpenAPIV3Schema }) {
   const normalizedSchemaName = pascalCase(args.schemaName);
   const typeOutput = toTypeScripType(args.schemaObject);
 
-  return `export type ${normalizedSchemaName} = ${typeOutput};`;
+  return `
+    ${typeOutput.comment ?? '\n'}
+    export type ${normalizedSchemaName} = ${typeOutput.output};
+  `;
 }
