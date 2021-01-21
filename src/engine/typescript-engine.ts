84df2e7171149c34eb7a956cbd5ce3a884224626
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-01-20 23:49:12 -0500

    add basic shit

--- src/engine/typescript-engine.ts
+++ src/engine/typescript-engine.ts
@@ -1,22 +1,39 @@
-import { BaseGeneratorType } from "./base-generator-type";
+import { BaseGeneratorType } from './base-generator-type';
 //import { OpenAPIV3Schema } from '../types';
 import { pascalCase } from 'change-case';
 
+function toTypeScripType(data: any) {
+  if (data.type === 'integer') {
+    return 'number';
+  }
+
+  if (data.type === 'array') {
+    return 'Array<any>';
+  }
+
+  return data.type;
+}
+
 export class TypeSrcriptEngine implements BaseGeneratorType {
-    private constructor() { }
+  private constructor() {}
 
-    private static driller(data: any): any {
-        let generate_type: any = {};
-        Object.keys(data).forEach(key => {
-            generate_type[key] = data[key]['type'];
-        })
-        return JSON.stringify(generate_type);
-    }
+  private static driller(data: any) {
+    const output: string[] = [];
 
-    public static generateTypes(args: { schemaName: string; schemaObject: any }): String {
-        const normalizedSchemaName = pascalCase(args.schemaName);
-        const properties = this.driller(args.schemaObject['properties']);
-        return `type ${normalizedSchemaName} = ${properties};\n`
-    }
+    Object.keys(data).forEach((key) => {
+      const type = toTypeScripType(data[key]);
 
+      output.push(`${key}: ${type}`);
+    });
+
+    return output.join(';');
+  }
+
+  public static generateTypes(args: { schemaName: string; schemaObject: any }) {
+    const normalizedSchemaName = pascalCase(args.schemaName);
+    const properties = this.driller(args.schemaObject['properties']);
+    return `export type ${normalizedSchemaName} = {
+      ${properties};
+    }`;
+  }
 }
